1fa5df899c7b9c3b13969bae4be4f608ccbbe6f5
stagiaire <rabahguittoune@gmail.com>
2016-12-13 11:46:45 +0100

    sql

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -18,7 +18,7 @@ public class InsultGenerator {
 				databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
 				databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
 				String username = System.getenv("POSTGRESQL_USER");
-				String password = System.getenv("PGPASSWORD");
+				String password = System.getenv("POSTGRESQL_PASSWORD");
 				Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
 
